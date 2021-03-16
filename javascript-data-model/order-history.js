4c17bf5b1e9178a68376bdd852c00d2017ee0a9f
Hien Nguyen <hnguy179@gmail.com>
2021-03-16 12:15:03 -0700

    completed javascript data model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,73 @@
+var orderHistory = [
+  {
+    orderPlaced: 'August 4, 2020',
+    total: '$34.00',
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    delivered: 'August 8, 2020',
+    itemDescription: [
+      {
+        image: '',
+        name: 'JavaScript for impatient programmers',
+        author: 'Dr Axel Rauschmayer',
+        returnDeadline: 'Sept 7, 2020',
+        price: '$31.55'
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 19, 2020',
+    total: '$44.53',
+    shipTo: 'JS Masher',
+    orderNumber: '113-99844268-1280257',
+    delivered: 'July 20, 2020',
+    itemDescription: [
+      {
+        image: '',
+        name: 'The Timeless Way of Building',
+        author: 'Christopher Alexander',
+        returnDeadline: 'August 19, 2020',
+        price: '$41.33'
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 4, 2020',
+    total: '$17.22',
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    delivered: 'July 7, 2020',
+    itemDescription: [
+      {
+        image: '',
+        name: 'Gamecube Controller Adapter',
+        author: null,
+        returnDeadline: 'August 5, 2020',
+        price: '$15.98'
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 3, 2020',
+    total: '$138.93',
+    shipTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    delivered: 'July 5, 2020',
+    itemDescription: [
+      {
+        image: '',
+        name: 'GameCube Controller',
+        author: null,
+        returnDeadline: 'August 4, 2020',
+        price: '$94.95'
+      },
+      {
+        image: '',
+        name: 'The Art of Sql',
+        author: 'Stephane Faroult',
+        returnDeadline: 'August 4, 2020',
+        price: '$33.99'
+      }
+    ]
+  }
+];
