e8c75c48a5fdc1334e671a2cfc6d87199fb0573d
Hien Nguyen <hnguy179@gmail.com>
2021-03-23 09:38:44 -0700

    fixed setValue and getValue functions

--- object-manipulation-1/src/get-value.js
+++ object-manipulation-1/src/get-value.js
@@ -1,9 +1,4 @@
 /* exported getValue */
 function getValue(object, key) {
-  for (var key2 in object) {
-    if (object[key] === object[key2]) {
-      return object[key];
-    }
-  }
-  return undefined;
+  return object[key];
 }
--- object-manipulation-1/src/set-value.js
+++ object-manipulation-1/src/set-value.js
@@ -1,8 +1,4 @@
 /* exported setValue */
 function setValue(object, key, value) {
   object[key] = value;
-  if (Object.keys(object).length !== 0) {
-    return object;
-  }
-  return undefined;
 }
