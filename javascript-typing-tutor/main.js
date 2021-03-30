8333c44d8ce1d40c63334ee87172d55f2f06133c
Hien Nguyen <hnguy179@gmail.com>
2021-03-30 09:38:44 -0700

    completed javascript typing tutor

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -4,11 +4,22 @@ var index = 0;
 $letters[index].classList.add('underline');
 
 document.addEventListener('keydown', function (event) {
+
   if (event.key === $letters[index].textContent) {
     $letters[index].className = 'correct';
     index++;
+  } else if ($letters[index].className === 'letter space underline' && event.code === 'Space') {
+    $letters[index].className = 'correct';
+    index++;
   } else {
-    $letters[index].className = 'incorrect';
+    if ($letters[index].className === 'letter space underline') {
+      $letters[index].className = 'letter space';
+    } else {
+      if ($letters[index].className === 'letter space underline') {
+        $letters[index].className = 'underline-incorrect';
+      }
+      $letters[index].className = 'incorrect';
+    }
   }
   $letters[index].classList.add('underline');
 });
