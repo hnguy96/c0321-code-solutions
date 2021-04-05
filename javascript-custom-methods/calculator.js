d32617b1515a5528a52ff7a08dd0f85760a40cf7
Hien Nguyen <hnguy179@gmail.com>
2021-04-05 11:23:59 -0700

    Completed javascript custom methods

---
+++ javascript-custom-methods/calculator.js
@@ -0,0 +1,34 @@
+/* exported calculator */
+var calculator = {
+  add: function (x, y) {
+    return x + y;
+  },
+  subtract: function (x, y) {
+    return x - y;
+  },
+  multiply: function (x, y) {
+    return x * y;
+  },
+  divide: function (x, y) {
+    return x / y;
+  },
+  square: function (x) {
+    return x * x;
+  },
+  sumAll: function (numbers) {
+    var total = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      total += numbers[i];
+    }
+    return total;
+  },
+  getAverage: function (numbers) {
+    var total = 0;
+    var averageNum = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      total += numbers[i];
+    }
+    averageNum = total / numbers.length;
+    return averageNum;
+  }
+};
