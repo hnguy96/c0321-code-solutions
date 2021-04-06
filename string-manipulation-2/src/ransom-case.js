613a2d5d429f13a40146f364e03c3815035b90bf
Hien Nguyen <hnguy179@gmail.com>
2021-04-05 21:50:45 -0700

    Finished ransomCase function

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,15 @@
 /* exported ransomCase */
+// assign a new word var
+// loop through string
+// capitalize every even word
+function ransomCase(string) {
+  var newStr = '';
+  for (var i = 0; i < string.length; i++) {
+    if (i % 2 === 0) {
+      newStr += string.charAt(i).toLowerCase();
+    } else {
+      newStr += string.charAt(i).toUpperCase();
+    }
+  }
+  return newStr;
+}
