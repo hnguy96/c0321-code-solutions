5c1adfe7f0303044bc3f376ddda8a3c1979504e7
Hien Nguyen <hnguy179@gmail.com>
2021-04-07 14:35:55 -0700

    Completed bank constructor

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,1 +1,36 @@
 /* exported Bank */
+function Bank(nextAccountNumber, accounts) {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (balance > 0 && Number.isInteger(balance)) {
+    var account = new Account(this.nextAccountNumber, holder);
+    account.transactions.push({ type: 'deposit', amount: balance });
+    this.accounts.push(account);
+    this.nextAccountNumber++;
+    return this.accounts.length;
+  }
+  return null;
+};
+
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  var totalAssets = 0;
+  if (this.accounts.length === 0) {
+    return 0;
+  }
+  for (var i = 0; i < this.accounts.length; i++) {
+    totalAssets += this.accounts[i].getBalance();
+  }
+  return totalAssets;
+};
