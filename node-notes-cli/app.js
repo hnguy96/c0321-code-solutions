5c89de275e6b1c1f59ca4db64d79231affce4746
Hien Nguyen <hnguy179@gmail.com>
2021-04-30 02:34:21 +0000

    Completed Add Note feature

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,13 +1,22 @@
 const fs = require('fs');
 const feature = process.argv[2];
+const newNote = process.argv[3];
 
-if (feature === 'read') {
-  fs.readFile('./data.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    data = JSON.parse(data);
-    const { notes } = data;
+fs.readFile('./data.json', 'utf8', (err, data) => {
+  if (err) throw err;
+  data = JSON.parse(data);
+  const { notes, nextId } = data;
+  if (feature === 'read') {
     for (const i in notes) {
       console.log(`${i}: ${notes[i]}`);
     }
-  });
-}
+  }
+  if (feature === 'create') {
+    notes[nextId] = newNote;
+    data.nextId++;
+    data = JSON.stringify(data, null, 2);
+    fs.writeFile('./data.json', data, 'utf8', err => {
+      if (err) throw err;
+    });
+  }
+});
