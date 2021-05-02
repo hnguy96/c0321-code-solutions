d827c31e3bbf747f42b1e9834cc63342e590e2d9
Hien Nguyen <hnguy179@gmail.com>
2021-05-02 16:12:38 -0700

    Completed capitalizeWords()

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -3,3 +3,14 @@
 // split each word into an array
 // capitalize first letter of each word
 // join the array
+function capitalizeWords(string) {
+  const wordsArr = string.toLowerCase().split(' ');
+  let capWords = '';
+  for (let i = 0; i < wordsArr.length; i++) {
+    capWords += wordsArr[i].charAt(0).toUpperCase() + wordsArr[i].slice(1, wordsArr[i].length);
+    if (i !== (wordsArr.length - 1)) {
+      capWords += ' ';
+    }
+  }
+  return capWords;
+}
