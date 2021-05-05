166a1057e43629c16ced28fe90b912e06c05d1e5
Hien Nguyen <hnguy179@gmail.com>
2021-05-05 19:15:33 +0000

    Completed clients can GET a single note by id

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -19,3 +19,17 @@ app.listen(3000, () => {
 app.get('/api/notes', (req, res) => {
   res.json(notes);
 });
+
+app.get('/api/notes/:id', (req, res) => {
+  if (req.params.id < 0) {
+    res.status(400).json({ error: 'ID must be a positive integer' });
+  } else {
+    for (let i = 0; i < notes.length; i++) {
+      if (req.params.id === notes[i].id.toString()) {
+        res.status(200).json(notes[i]);
+      } else {
+        res.status(404).json({ error: `cannot find note with id ${req.params.id}` });
+      }
+    }
+  }
+});
