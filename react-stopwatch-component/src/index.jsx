495c6bbf05cbb6444d4e100bbd108cceb3f0778f
Hien Nguyen <hnguy179@gmail.com>
2021-05-18 17:58:20 +0000

    Saved changes

--- react-stopwatch-component/src/index.jsx
+++ react-stopwatch-component/src/index.jsx
@@ -4,29 +4,57 @@ import ReactDOM from 'react-dom';
 class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { timer: 0 };
-    this.tick = this.tick.bind(this);
+    this.state = { timer: 0, isIconClicked: false, isStopwatchClicked: false };
+    this.startTimer = this.startTimer.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.pauseTimer = this.pauseTimer.bind(this);
+    this.clearTimer = this.clearTimer.bind(this);
   }
 
-  // componentDidMount() {
-  //   this.timerId = setInterval(
-  //     () => this.tick(),
-  //     1000
-  //   );
-  // }
+  handleClick() {
+    if (!this.state.isIconClicked) {
+      this.timerId = setInterval(
+        () => this.startTimer(),
+        1000
+      );
+      this.setState({ isIconClicked: !this.state.isIconClicked });
+    } else {
+      clearInterval(this.timerId);
+      this.setState({ isStopwatchClicked: !this.state.isStopwatchClicked });
+      this.pauseTimer();
+    }
+  }
 
-  tick() {
-    this.timerId = setInterval(
-      () => this.tick(),
-      1000
-    );
+  startTimer() {
+    this.setState({ timer: this.state.timer + 1 });
+  }
+
+  pauseTimer() {
+    this.setState({ isIconClicked: !this.state.isIconClicked });
+  }
+
+  clearTimer() {
+    this.setState({ timer: 0, isStopwatchClicked: !this.state.isStopwatchClicked });
   }
 
   render() {
+    const isIconClicked = this.state.isIconClicked;
+    const isStopwatchClicked = this.state.isStopwatchClicked;
+    let icon;
+    let button;
+    button = <button>{ this.state.timer }</button >;
+    if (isIconClicked) {
+      icon = <i onClick={this.handleClick} className="pauseButton fa fa-pause"></i>;
+    } else {
+      icon = <i onClick={this.handleClick} className="playButton fa fa-play"></i>;
+      if (isStopwatchClicked) {
+        button = <button onClick={this.clearTimer}>{this.state.timer}</button>;
+      }
+    }
     return (
-      <div>
-        {this.state.timer}
-        <i onClick={this.tick} className="fas fa-play"></i>
+      <div className="stopwatchContainer">
+        {button}
+        {icon}
       </div>
     );
   }
