04be4120e7f19375a6b03e07b6db421c7ac79322
Hien Nguyen <hnguy179@gmail.com>
2021-06-30 22:07:24 +0000

    Updated stylings

--- react-carousel-component/src/images.jsx
+++ react-carousel-component/src/images.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Images() {
+export default function Images(props) {
   const numbers = ['001', '004', '007', '025', '039'];
   const images = numbers.map(num => {
     return <img key={num} src={`../dist/images/${num}.png`} className="pokemon" />;
